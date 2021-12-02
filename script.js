f033735bfb6518214290a4094b227a62dd1f67dc
JairoSolarteRodriguez <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2021-12-02 13:32:44 -0500

    busqueda del indice donde esta la letra

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const contenedor = document.querySelector('.contenedor__letras');
-const palabras = ['gato', 'juego', 'plato', 'camino', 'jirafa'];
+const palabras = ['gato', 'juego', 'plato', 'camino', 'jirafa', 'cooperar'];
 
 // Obtener palabra random de la lista
 let palabra = palabras[Math.floor((Math.random() * palabras.length))];
 
 // Palabra dividida
-let arrPalabra = new String(palabra);
+// let arrPalabra = new String(palabra);
 
 // Botton para probar palabra
 let probar = document.querySelector("#probar");
@@ -13,22 +13,35 @@ probar.addEventListener("click", procesar);
 
 // Validar si la letra existe en la palabra
 function procesar(){
-    let letra = document.querySelector("#text").value;
-    for(let a in arrPalabra){
-        if(letra == arrPalabra[a]){
-            console.log('Bien');
-        }
+    let campoLetra = document.querySelector("#text");
+    let letra = campoLetra.value;
+
+    let indices = [];
+    let indice = palabra.indexOf(letra);
+
+    console.log(indice);
+
+    while(indice != -1){
+        indices.push(indice);
+        indice = palabra.indexOf(letra, indice + 1);
     }
+
+    console.log(indices);
 }
-
-
 
 console.log(palabra);
 // Dibuja las lineas en pantalla
 for(let p in palabra){
-    contenedor.innerHTML += `<div class="contenedor__letras-o"><h1></h1></div>`;
+    contenedor.innerHTML += `<div class="contenedor__letras-o letra"><h1 value='${p}'></h1></div>`;
 }
 
+// prueba
+let l = document.getElementsByClassName('letra');
+
+l[0].innerText= 'a';
+
+
+//===========================
 
 
 
