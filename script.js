76f3d940c8a77c04f749cadeb0bceca3b7cb3d25
JairoSolarteRodriguez <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2021-12-02 11:44:49 -0500

    palabras random

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const contenedor = document.querySelector('.contenedor__letras');
+const palabras = ['gato', 'juego', 'plato', 'camino', 'jirafa'];
+const palabra = palabras[Math.ceil((Math.random() * palabras.length))];
+
+console.log(palabra);
+for(let p in palabra){
+    contenedor.innerHTML += `<div class="contenedor__letras-o"><h1></h1></div>`;
+}
+
+
+
 // alert("este es  tu juego")
-alert('prueba de ramas');
+// const palabra =["j","u","e","g","o"]
 
-const palabra =["j","u","e","g","o"]
-
-let probar=document.querySelector("#probar")
-probar.addEventListener("click",procesando)
+// let probar=document.querySelector("#probar")
+// probar.addEventListener("click", procesando);
 
 
 function procesando(){
-    let letra=document.querySelector("#text").value;
+    let letra = document.querySelector("#text").value;
     for(const i in palabra){
-        if(letra==palabra[i]){
+        if(letra == palabra[i]){
             alert("la letra si se encuentra dentro de la palabra en la posicion")
             break
         }
