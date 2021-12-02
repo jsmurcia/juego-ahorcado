ee8f87bac86981a61129a2125e125fc81bc85450
JairoSolarteRodriguez <68309004+JairoSolarteRodriguez@users.noreply.github.com>
2021-12-02 13:55:17 -0500

    Colocacion de las letras y estilos para que se vean grandes

--- script.js
+++ script.js
@@ -1,83 +1,41 @@
+// Contenedor de la palabra y palabras para el juego
 const contenedor = document.querySelector('.contenedor__letras');
 const palabras = ['gato', 'juego', 'plato', 'camino', 'jirafa', 'cooperar'];
-
-// Obtener palabra random de la lista
-let palabra = palabras[Math.floor((Math.random() * palabras.length))];
-
-// Palabra dividida
-// let arrPalabra = new String(palabra);
 
 // Botton para probar palabra
 let probar = document.querySelector("#probar");
 probar.addEventListener("click", procesar);
 
-// Validar si la letra existe en la palabra
+// Obtener palabra random de la lista
+let palabra = palabras[Math.floor((Math.random() * palabras.length))];
+
+// Recuardros de las letras
+let Posiciones = document.getElementsByClassName('letra');
+
+// Validar si la letra existe en la palabra y colocarla en la posicion
 function procesar(){
     let campoLetra = document.querySelector("#text");
-    let letra = campoLetra.value;
+    let letra = campoLetra.value.toLowerCase();
 
+    //Buscar el indice donde se encuentra la letra
     let indices = [];
     let indice = palabra.indexOf(letra);
-
-    console.log(indice);
-
     while(indice != -1){
         indices.push(indice);
         indice = palabra.indexOf(letra, indice + 1);
     }
 
-    console.log(indices);
+    // Coloca la palabra en el lugar que corresponde
+    for(let i in indices){
+        console.log(indices[i]);
+        Posiciones[indices[i]].innerText = letra;
+    }
+    // Borra el formulario
+    campoLetra.value = null;
 }
 
-console.log(palabra);
 // Dibuja las lineas en pantalla
+console.log(palabra);
 for(let p in palabra){
     contenedor.innerHTML += `<div class="contenedor__letras-o letra"><h1 value='${p}'></h1></div>`;
 }
-
-// prueba
-let l = document.getElementsByClassName('letra');
-
-l[0].innerText= 'a';
-
-
-//===========================
-
-
-
-// alert("este es  tu juego")
-// const palabra =["j","u","e","g","o"]
-
-
-
-
-// function procesando(){
-//     let letra = document.querySelector("#text").value;
-//     for(const i in palabra){
-//         if(letra == palabra[i]){
-//             alert("la letra si se encuentra dentro de la palabra en la posicion")
-//             break
-//         }
-//         else if ( letra !== palabra[i]){
-//             alert("la letra no se encuentra dentro de la palabra")
-//             break
-//         }
-//     }
-//     console.log(letra)
-// }
-
-// alert("este es  tu juego")
-// document.querySelector("#probar").addEventListener("click", ()=>{
-//     alert("vamos bien")
-// })
-
-
-// function procesando(){
-//     let letra=document.querySelector("#text").value;
-//     switch (letra){
-//         case letra=="j":
-//             document.querySelector( "#contenedor__letras-j-h").innerHTML="j"
-            
-//     }
-
-// }
